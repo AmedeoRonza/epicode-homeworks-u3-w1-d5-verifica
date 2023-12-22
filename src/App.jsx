a65b0b99acb184302a507808785c033d4553f083
Amedeo Ronza <amedeoronza@gmail.com>
2023-12-22 14:04:53 +0100

    ErFootFatto

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Topbar from "./Components/Topbar";
 import SubTopbar from "./Components/SubTopbar";
+import ErFoot from "./Components/ErFoot";
 
 function App() {
 	return (
 		<div className="App">
 			<Topbar />
 			<SubTopbar />
+			<ErFoot />
 		</div>
 	);
 }
